db9d7ee5556c0e5086e5fb98efa2e963c88a10a9
Tan Duc <ductan10092003@gmail.com>
2022-04-13 00:11:54 +0700

    Test

--- painter.cpp
+++ painter.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "painter.h"
 
 /***
@@ -9,6 +10,8 @@
 void Painter::setColor(SDL_Color color) 
 { 
     // TODO: set the color value for the Painter and set Render Draw Color
+    this->color = color;
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 0);
 }
 
 
@@ -21,6 +24,9 @@ void Painter::setColor(SDL_Color color)
 void Painter::jumpForward(int numPixel)
 {
     // TODO: jump the painter forward
+    double rad = (angle / 180) * M_PI;
+    x += cos(rad) * numPixel;
+    y -= sin(rad) * numPixel;
 }
 
 
@@ -33,6 +39,7 @@ void Painter::jumpForward(int numPixel)
 void Painter::jumpBackward(int numPixel)
 {
     // TODO: jump the painter backward
+    jumpForward(-numPixel);
 }
 
 
@@ -44,7 +51,8 @@ void Painter::jumpBackward(int numPixel)
 ***/       
 void Painter::turnLeft(double degree)
 {
-    // TODO: rotate left the painter   
+    // TODO: rotate left the painter
+    angle -= degree * M_PI / 180;
 }
 
 
@@ -56,7 +64,8 @@ void Painter::turnLeft(double degree)
 ***/     
 void Painter::turnRight(double degree)
 {
-    // TODO: rotate right the painter   
+    // TODO: rotate right the painter
+    turnLeft(-degree);
 }
 
 /***  
@@ -67,7 +76,10 @@ void Painter::turnRight(double degree)
 ***/
 void Painter::randomColor()
 {
-    // TODO: set random color    
+    // TODO: set random color
+    this->color.r = rand() % 256;
+    this->color.g = rand() % 256;
+    this->color.b = rand() % 256;
 }
 
 
