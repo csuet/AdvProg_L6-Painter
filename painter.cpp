04fc06ae943d35c6695a6b56b0cf6036f4d76444
hoang1803 <21020073@vnu.edu.vn>
2022-04-13 17:18:24 +0700

    Magic

--- painter.cpp
+++ painter.cpp
@@ -10,7 +10,6 @@ void Painter::setColor(SDL_Color color)
 { 
     // TODO: set the color value for the Painter and set Render Draw Color
     this -> color = color;
-    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 0);
 }
 
 
