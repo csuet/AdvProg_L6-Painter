dd7acf06781a6da22486c94eb35235b6129b7000
Trinh Van Khanh <21020343@vnu.edu.vn>
2022-04-24 17:33:53 +0700

    21020343

--- painter.cpp
+++ painter.cpp
@@ -10,7 +10,7 @@ void Painter::setColor(SDL_Color color)
 { 
     // TODO: set the color value for the Painter and set Render Draw Color
     color = this->color;
-    SDL_setRenderDrawColor(renderer, color);
+    SDL_SetRenderDrawColor(renderer,color);
 }
 
 
