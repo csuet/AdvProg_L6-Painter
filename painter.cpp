264f17f2fc0784329e18695c5a8ad9123ab35848
NguyenTienManh2409 <21020777@vnu.edu.vn>
2022-04-24 17:38:57 +0700

    21020777 Nguyen Tien Manh

--- painter.cpp
+++ painter.cpp
@@ -1,86 +1,37 @@
 #include "painter.h"
 #include <SDL2/SDL.h>
 
-/***
-    Args: color (SDL_Color): color value 
-        
-    Returns:
-        None
-***/
+
 void Painter::setColor(SDL_Color color) 
 { 
-    // TODO: set the color value for the Painter and set Render Draw Color
     this->color = color;
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 0);
 }
 
-void Painter::setAngle(float angle)
-{
-    this->angle = angle - floor(angle/360)*360;
-}
-
-/***
-    Args: numPixel (int): number of pixel for jumping forward
-        
-    Returns:
-        None
-***/
 void Painter::jumpForward(int numPixel)
 {
-    // TODO: jump the painter forward
     float rad = (angle / 180) * M_PI;
     x += cos(rad) * numPixel;
     y -= sin(rad) * numPixel;
 }
 
-
-/***
-    Args: numPixel (int): number of pixel for jumping backward
-        
-    Returns:
-        None
-***/
 void Painter::jumpBackward(int numPixel)
 {
-    // TODO: jump the painter backward
     jumpForward(- numPixel);
 }
-
-
-/***
-    Args: degree (double): the value of rotation angle
-        
-    Returns:
-        None
-***/       
+  
 void Painter::turnLeft(double degree)
-{
-    // TODO: rotate left the painter   
-    setAngle(this->angle + degree); 
+{ 
+    setAngle(this->angle + degree* M_PI / 180;); 
 }
 
-
-/***
-    Args: degree (double): the value of rotation angle
-        
-    Returns:
-        None
-***/     
 void Painter::turnRight(double degree)
 {
-    // TODO: rotate right the painter   
     turnLeft(-degree);
 }
 
-/***  
-    Args: 
-        None
-    Returns:
-        None
-***/
 void Painter::randomColor()
-{
-    // TODO: set random color    
+{    
     Uint8 r = rand() % 256;
     Uint8 g = rand() % 256;
     Uint8 b = rand() % 256;
