9e75c4a66036d15fd81d0a6ecc822939a05a6ff4
hongquant17 <21020555@vnu.edu.vn>
2022-04-25 21:59:27 +0700

    pAiNtEr

--- painter.cpp
+++ painter.cpp
@@ -9,6 +9,7 @@
 void Painter::setColor(SDL_Color color) 
 { 
     // TODO: set the color value for the Painter and set Render Draw Color
+    this->color = color;
 }
 
 
@@ -21,6 +22,7 @@ void Painter::setColor(SDL_Color color)
 void Painter::jumpForward(int numPixel)
 {
     // TODO: jump the painter forward
+    this->x += numPixel;
 }
 
 
@@ -33,6 +35,7 @@ void Painter::jumpForward(int numPixel)
 void Painter::jumpBackward(int numPixel)
 {
     // TODO: jump the painter backward
+    this->x -= numPixel;
 }
 
 
@@ -44,7 +47,8 @@ void Painter::jumpBackward(int numPixel)
 ***/       
 void Painter::turnLeft(double degree)
 {
-    // TODO: rotate left the painter   
+    // TODO: rotate left the painter
+    this->angle -= degree;
 }
 
 
@@ -57,6 +61,7 @@ void Painter::turnLeft(double degree)
 void Painter::turnRight(double degree)
 {
     // TODO: rotate right the painter   
+    this->angle += degree;
 }
 
 /***  
@@ -67,7 +72,10 @@ void Painter::turnRight(double degree)
 ***/
 void Painter::randomColor()
 {
-    // TODO: set random color    
+    // TODO: set random color   
+    this->color.r = rand() % 256;
+    this->color.g = rand() % 256;
+    this->color.b = rand() % 256; 
 }
 
 
