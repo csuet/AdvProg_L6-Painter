e1cb38406746e31ca045554df2407198c5f0f529
VuThiThanhVinh <21020422@vnu.edu.vn>
2022-04-25 22:32:29 +0700

    21020422

--- painter.h
+++ painter.h
@@ -7,9 +7,9 @@
 using namespace std;
 
 //Mac
-//#include <SDL2/SDL.h>
+#include <SDL2/SDL.h>
 //Windows
- #include <SDL.h>
+ //#include <SDL.h>
 
 const SDL_Color CYAN_COLOR = {0, 255, 255};
 const SDL_Color BLUE_COLOR = {0, 0, 255};
