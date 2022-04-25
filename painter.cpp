48e2f0be21594700196ce6551e62db626f347fbc
21020541_NguyenTienHung <100121387+NguyenTienHung2109@users.noreply.github.com>
2022-04-25 22:44:37 +0700

    Update

--- painter.cpp
+++ painter.cpp
@@ -3,55 +3,99 @@
 
 using namespace std;
 
-void Painter::setColor(SDL_Color color)
-{
-void Painter::setColor(SDL_Color color)
-{
+/***
+    Args: color (SDL_Color): color value
 
+    Args: color (SDL_Color): color value
+    Returns:
+        None
+***/
+void Painter::setColor(SDL_Color color)
+{
+void Painter::setColor(SDL_Color color)
+{
+    // TODO: set the color value for the Painter and set Render Draw Color
     this -> color = color;
     SDL_setRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
 }
 
+/***
+    Args: numPixel (int): number of pixel for jumping forward
 
+    Returns:
+        None
+***/
 void Painter::jumpForward(int numPixel)
 {
+    // TODO: jump the painter forward
     float rad = (angle /180) *M_PI;
     x+= numPixel * cos(rad);
     y-= numPixel * sin(rad);
 }
 
+/***
+    Args: numPixel (int): number of pixel for jumping backward
 
+    Returns:
+        None
+***/
 void Painter::jumpBackward(int numPixel)
 {
-
+    // TODO: jump the painter backward
 	painter.jumpForward(-numPixel);
 }
 
+/***
+    Args: degree (double): the value of rotation angle
 
+    Returns:
+        None
+***/
+***/
 void Painter::turnLeft(double degree)
 {
-
+    // TODO: rotate left the painter
+    // TODO: rotate left the painter
 	angle += degree;
 	angle -= floor(angle/360) * 360;
 }
 
 
+/***
+    Args: degree (double): the value of rotation angle
 
+    Returns:
+        None
+***/
+***/
 void Painter::turnRight(double degree)
 {
+    // TODO: rotate right the painter
+    // TODO: rotate right the painter
 	painter.turnLeft(-degree);
 }
 
+/***
+    Args:
+/***
+    Args:
+        None
+    Returns:
+        None
+***/
 void Painter::randomColor()
 {
-
+    // TODO: set random color
+    // TODO: set random color
     Uint8 r = rand() % 256;
     Uint8 g = rand() % 256;
     Uint8 b = rand() % 256;
     SDL_Color color = {r,g,b};
     painter.setColor(color);
 }
-
+/***
+Part of code that not need to be implemented
+***/
 void Painter::clearWithBgColor(SDL_Color bgColor)
 {
     SDL_Color curColor = color;
