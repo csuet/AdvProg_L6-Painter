6371d884db231e84d6daa150de6a3b1770c588b2
minh0010 <21020778@vnu.edu.vn>
2022-04-25 22:24:25 +0700

    update 3

--- utils.h
+++ utils.h
@@ -2,19 +2,19 @@
 #define UTILS_H
 
 #include <iostream>
-#include <SDL2/SDL.h>
+#include <SDL.h>
 
-const int SCREEN_WIDTH  = 800;
+const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
 const std::string WINDOW_TITLE = "An Implementation of Code.org Painter";
 
 SDL_Color dict2SDL_Color(int r, int g, int b);
 bool validColorValue(int value);
-void initSDL(SDL_Window **window, SDL_Renderer **renderer);
-void quitSDL(SDL_Window **window, SDL_Renderer **renderer);
+void initSDL(SDL_Window** window, SDL_Renderer** renderer);
+void quitSDL(SDL_Window** window, SDL_Renderer** renderer);
 void waitUntilKeyPressed();
-void logSDLError(std::ostream& os, const std::string &msg);
+void logSDLError(std::ostream& os, const std::string& msg);
 
 
 #endif
