977c8e2f12b1e3c8aedf68f40443e371f9f2bf14
tuananhtran21020280 <100192298+tuananhtran21020280@users.noreply.github.com>
2022-04-26 10:47:28 +0700

    Update painter.cpp

--- painter.cpp
+++ painter.cpp
@@ -9,6 +9,7 @@
 void Painter::setColor(SDL_Color color) 
 { 
     // TODO: set the color value for the Painter and set Render Draw Color
+     this->color = color;
 }
 
 
@@ -21,6 +22,9 @@ void Painter::setColor(SDL_Color color)
 void Painter::jumpForward(int numPixel)
 {
     // TODO: jump the painter forward
+     this->x += numPixel * cos(this->angle)
+     
+     this->y += numPixel * sin(this->angle);
 }
 
 
@@ -33,6 +37,7 @@ void Painter::jumpForward(int numPixel)
 void Painter::jumpBackward(int numPixel)
 {
     // TODO: jump the painter backward
+      this->jumpForward(-numPixel);   
 }
 
 
@@ -45,6 +50,7 @@ void Painter::jumpBackward(int numPixel)
 void Painter::turnLeft(double degree)
 {
     // TODO: rotate left the painter   
+      this->turnLeft(-degree);
 }
 
 
@@ -57,6 +63,7 @@ void Painter::turnLeft(double degree)
 void Painter::turnRight(double degree)
 {
     // TODO: rotate right the painter   
+      this->turnRight(-degree);
 }
 
 /***  
@@ -68,6 +75,8 @@ void Painter::turnRight(double degree)
 void Painter::randomColor()
 {
     // TODO: set random color    
+     this->color= rand();
+     
 }
 
 
