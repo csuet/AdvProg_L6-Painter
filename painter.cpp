eb260a6d20fe74b57eb201cd084fe3e69cd0d78b
guenfon <92385828+guenfon@users.noreply.github.com>
2022-04-26 08:36:29 +0700

    21020085_Nguyen Hai Phong

--- painter.cpp
+++ painter.cpp
@@ -9,6 +9,8 @@
 void Painter::setColor(SDL_Color color) 
 { 
     // TODO: set the color value for the Painter and set Render Draw Color
+    this->color = color;
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
 }
 
 
@@ -21,6 +23,7 @@ void Painter::setColor(SDL_Color color)
 void Painter::jumpForward(int numPixel)
 {
     // TODO: jump the painter forward
+    this->x += numPixel;
 }
 
 
@@ -33,6 +36,7 @@ void Painter::jumpForward(int numPixel)
 void Painter::jumpBackward(int numPixel)
 {
     // TODO: jump the painter backward
+    this->x -= numPixel;
 }
 
 
@@ -44,7 +48,8 @@ void Painter::jumpBackward(int numPixel)
 ***/       
 void Painter::turnLeft(double degree)
 {
-    // TODO: rotate left the painter   
+    // TODO: rotate left the painter 
+    this->angle -= degree;  
 }
 
 
@@ -57,6 +62,7 @@ void Painter::turnLeft(double degree)
 void Painter::turnRight(double degree)
 {
     // TODO: rotate right the painter   
+    this->angle += degree;
 }
 
 /***  
@@ -67,7 +73,13 @@ void Painter::turnRight(double degree)
 ***/
 void Painter::randomColor()
 {
-    // TODO: set random color    
+    // TODO: set random color 
+    this->color.r = rand() % 255;
+    this->color.g = rand() % 255;
+    this->color.b = rand() % 255;
+    this->color.a = 255;
+    // set the color value for the Painter and set Render Draw Color
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
 }
 
 
