62c87e823f81f62d2e2db6347324ef443d046a3f
HNgan <hnagn2003@gmail.com>
2022-05-11 16:55:01 +0700

    awkjhd

--- painter.cpp
+++ painter.cpp
@@ -8,7 +8,8 @@
 ***/
 void Painter::setColor(SDL_Color color) 
 { 
-    // TODO: set the color value for the Painter and set Render Draw Color
+    this->color = color;
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 0);
 }
 
 
@@ -20,7 +21,9 @@ void Painter::setColor(SDL_Color color)
 ***/
 void Painter::jumpForward(int numPixel)
 {
-    // TODO: jump the painter forward
+    float rad = (angle / 180) * M_PI;
+    x += cos(rad) * numPixel;
+    y -= sin(rad) * numPixel;
 }
 
 
@@ -32,6 +35,7 @@ void Painter::jumpForward(int numPixel)
 ***/
 void Painter::jumpBackward(int numPixel)
 {
+    jumpForward(-numPixel);
     // TODO: jump the painter backward
 }
 
@@ -44,6 +48,7 @@ void Painter::jumpBackward(int numPixel)
 ***/       
 void Painter::turnLeft(double degree)
 {
+    setAngle(this->angle + degree);
     // TODO: rotate left the painter   
 }
 
@@ -56,6 +61,7 @@ void Painter::turnLeft(double degree)
 ***/     
 void Painter::turnRight(double degree)
 {
+    turnLeft(-degree);
     // TODO: rotate right the painter   
 }
 
@@ -67,7 +73,11 @@ void Painter::turnRight(double degree)
 ***/
 void Painter::randomColor()
 {
-    // TODO: set random color    
+    Uint8 r = rand() % 256;
+    Uint8 g = rand() % 256;
+    Uint8 b = rand() % 256;
+    SDL_Color color = { r, g, b };
+    setColor(color);// TODO: set random color    
 }
 
 
